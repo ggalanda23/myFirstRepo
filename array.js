958c6805df23c2d9f0bb134c25dfee6f1685d7a1
alexander landa <ggalanda23@gmail.com>
2022-05-01 20:49:47 +0300

    my first commit

---
+++ array.js
@@ -0,0 +1,70 @@
+//let i =0;
+//while (i < arr.length){
+//    console.log(arr[i]);
+//    i = i + 1
+//}
+//console.log("Finish while loop");
+
+
+/*let arr = [15,32,45];                 מדלג כאשר יש continue
+for (let i = 0, i < 10, i++){
+    if (i==4){
+        continue;
+    }
+    console.log(i);
+    }
+}
+/console.log("Finish The Loop");*/
+
+
+/*let fruits = ["apple","banana","orange"];
+for (let i = 0; i < fruits.length; i++)*/
+
+
+/*let fruits = ["apple","banana","orange"];
+for (let i = 0, i < fruits.length; i++){
+    console.log("We have Bananas");
+}
+console.log(fruits(i));
+}*/
+
+
+
+// // OBJECT
+
+// let person = {
+//     name: 'John',
+//     age: 40,
+//     country: 'Israel',
+//     sayHello: () => console.log("Hello"),
+// };
+// console.log(person);
+
+// console.log(sayHello);
+
+// console.log(person.age);     // print the age in the array //
+
+// console.log(person["name"])  // print the name value , the string //
+
+
+
+// adding another value to array //
+// let arr = [23,45,88];
+// console.log(arr);
+// arr.push(65);                         הוספת ערך למערך
+// console.log(arr);
+
+
+//arr.pop();          deleting last index in the array 
+//console.log(arr.includes(45));  if 45 exist in array >> will show TRUE
+
+ const arr = [23,45,88];
+// function isElementInArray(el, arr){
+//    return arr.includes(el);
+// }
+// console.log(isElementInArray(23,arr));
+
+
+
+arr.forEach(el => {console.log(el)});
+
---
+++ script.js
@@ -0,0 +1,28 @@
+/*function convertminutetosecond(minute){
+    return minute * 60 ;
+}
+console.log(convertminutetosecond(10));*/
+
+//+1 to number//
+/*function plus1(number){
+    return number + 1 ;
+}
+console.log(plus1(100));*/
+
+
+//triangle base and height to area//
+/*function trianglearea(height,base){
+    return (height * base)/2 ;
+}
+console.log(trianglearea(14,12));*/
+
+
+//years convert to days
+/*function yearstodays(years){
+    return years * 365 ;
+
+}
+console.log(yearstodays(3));*/
+
+
+//
